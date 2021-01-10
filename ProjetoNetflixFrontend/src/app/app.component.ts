afa72491fe481bb36d2e1b40eca81eab7bf115d6
Jhonathan <jhonathandosreis@hotmail.com>
2021-01-10 03:59:58 -0300

    Menu a caminho

--- ProjetoNetflixFrontend/src/app/app.component.ts
+++ ProjetoNetflixFrontend/src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ProjetoNetflix';
+    title = 'Netflix';
+
+    items: MenuItem[] = [];
+
+    ngOnInit() {
+        this.items = [
+            {label: 'Inicio',},
+            {label: 'Séries',},
+            {label: 'Filmes',},
+            {label: 'Bombando',},
+            {label: 'Minha Lista',}
+        ];
+    }
 }
